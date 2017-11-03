643fb8f99b54d71ba0580c7a63382d9737b756bc
Sandro Simas <sandro.csimas@gmail.com>
2017-11-03 04:11:56 -0300

    Adding script to create symbolic links to certificated

---
+++ nginx/link-certs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ -z "${APPS_HOME}" ]]; then
+  echo "Environment variable APPS_HOME is required"
+  exit 1
+fi
+
+sudo ln -s $APPS_HOME/chatz-configs/nginx/chatz.io/cert.pem /etc/letsencrypt/live/chatz.io/cert.pem
+sudo ln -s $APPS_HOME/chatz-configs/nginx/chatz.io/chain.pem /etc/letsencrypt/live/chatz.io/chain.pem
+sudo ln -s $APPS_HOME/chatz-configs/nginx/chatz.io/fullchain.pem /etc/letsencrypt/live/chatz.io/fullchain.pem
+sudo ln -s $APPS_HOME/chatz-configs/nginx/chatz.io/privkey.pem /etc/letsencrypt/live/chatz.io/privkey.pem
+
+sudo ln -s $APPS_HOME/chatz-configs/nginx/api.chatz.io/cert.pem /etc/letsencrypt/live/api.chatz.io/cert.pem
+sudo ln -s $APPS_HOME/chatz-configs/nginx/api.chatz.io/chain.pem /etc/letsencrypt/live/api.chatz.io/chain.pem
+sudo ln -s $APPS_HOME/chatz-configs/nginx/api.chatz.io/fullchain.pem /etc/letsencrypt/live/api.chatz.io/fullchain.pem
+sudo ln -s $APPS_HOME/chatz-configs/nginx/api.chatz.io/privkey.pem /etc/letsencrypt/live/api.chatz.io/privkey.pem
+
+sudo ln -s $APPS_HOME/chatz-configs/nginx/www.chatz.io/cert.pem /etc/letsencrypt/live/www.chatz.io/cert.pem
+sudo ln -s $APPS_HOME/chatz-configs/nginx/www.chatz.io/chain.pem /etc/letsencrypt/live/www.chatz.io/chain.pem
+sudo ln -s $APPS_HOME/chatz-configs/nginx/www.chatz.io/fullchain.pem /etc/letsencrypt/live/www.chatz.io/fullchain.pem
+sudo ln -s $APPS_HOME/chatz-configs/nginx/www.chatz.io/privkey.pem /etc/letsencrypt/live/www.chatz.io/privkey.pem
