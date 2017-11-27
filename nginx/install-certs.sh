403db683be03ef924b9000d0f1e8c0ebcafdf3d8
Sandro Simas <sandro.csimas@gmail.com>
2017-11-26 22:52:32 -0300

    Renewing webcm certificate

--- nginx/install-certs.sh
+++ nginx/install-certs.sh
@@ -9,5 +9,8 @@ sudo /opt/applications/letsencrypt/letsencrypt-auto certonly -d api.ayro.io --st
 echo "Installing www.ayro.io certificate"
 sudo /opt/applications/letsencrypt/letsencrypt-auto certonly -d www.ayro.io --standalone --cert-path /opt/applications/ayro-configs/nginx/www.ayro.io/cert.pem --key-path /opt/applications/ayro-configs/nginx/www.ayro.io/privkey.pem --chain-path /opt/applications/ayro-configs/nginx/www.ayro.io/chain.pem --fullchain-path /opt/applications/ayro-configs/nginx/www.ayro.io/fullchain.pem
 
+echo "Installing webcm.ayro.io certificate"
+sudo /opt/applications/letsencrypt/letsencrypt-auto certonly -d webcm.ayro.io --standalone --cert-path /opt/applications/ayro-configs/nginx/webcm.ayro.io/cert.pem --key-path /opt/applications/ayro-configs/nginx/webcm.ayro.io/privkey.pem --chain-path /opt/applications/ayro-configs/nginx/webcm.ayro.io/chain.pem --fullchain-path /opt/applications/ayro-configs/nginx/webcm.ayro.io/fullchain.pem
+
 echo "Certificates installed with success"
 exit 0
