888b3fd2ae9131999da3f90f15c2ac9ce527ff15
Sandro Simas <sandro.csimas@gmail.com>
2017-11-30 01:16:43 -0300

    Improving scripts

--- docker/install-docker-compose.sh
+++ docker/install-docker-compose.sh
@@ -7,4 +7,4 @@ echo "Installing Docker Compose..."
 sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-echo "Docker Compose installed with success!
+echo "Docker Compose installed with success!"
