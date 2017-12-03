5d908ac90bab1d63116933f07bb9d760fe98a086
Sandro Simas <sandro.csimas@gmail.com>
2017-12-03 12:05:17 -0300

    Improving create-user script to create group docker and add user to it

--- create-user.sh
+++ create-user.sh
@@ -6,5 +6,7 @@ echo "Creating user ayro"
 
 sudo adduser ayro
 sudo usermod -aG sudo ayro
+sudo groupadd docker || true
+sudo usermod -aG docker ayro
 
 echo "User ayro created with success!"
--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -16,7 +16,4 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get install docker-ce
 
-sudo groupadd docker || true
-sudo usermod -aG docker $USER
-
 echo "Docker installed with success!"
