30829676d2d12e23f3a0e61e02084e60724e5a1a
Sandro Simas <sandro.csimas@gmail.com>
2017-12-03 21:56:21 -0300

    Adding version to docker-compose services

--- certs/install-certs.sh
+++ certs/install-certs.sh
@@ -19,10 +19,10 @@ sudo certbot certonly -d ayro.io --standalone \
   --chain-path $AYRO_HOME/apps/ayro-configs/certs/ayro.io/chain.pem \
   --fullchain-path $AYRO_HOME/apps/ayro-configs/certs/ayro.io/fullchain.pem
 
-sudo ln -s /etc/letsencrypt/live/ayro.io/cert.pem $AYRO_HOME/apps/ayro-configs/certs/ayro.io/cert.pem
-sudo ln -s /etc/letsencrypt/live/ayro.io/privkey.pem $AYRO_HOME/apps/ayro-configs/certs/ayro.io/privkey.pem
-sudo ln -s /etc/letsencrypt/live/ayro.io/chain.pem $AYRO_HOME/apps/ayro-configs/certs/ayro.io/chain.pem
-sudo ln -s /etc/letsencrypt/live/ayro.io/fullchain.pem $AYRO_HOME/apps/ayro-configs/certs/ayro.io/fullchain.pem
+#sudo ln -s /etc/letsencrypt/live/ayro.io/cert.pem $AYRO_HOME/apps/ayro-configs/certs/ayro.io/cert.pem
+#sudo ln -s /etc/letsencrypt/live/ayro.io/privkey.pem $AYRO_HOME/apps/ayro-configs/certs/ayro.io/privkey.pem
+#sudo ln -s /etc/letsencrypt/live/ayro.io/chain.pem $AYRO_HOME/apps/ayro-configs/certs/ayro.io/chain.pem
+#sudo ln -s /etc/letsencrypt/live/ayro.io/fullchain.pem $AYRO_HOME/apps/ayro-configs/certs/ayro.io/fullchain.pem
 
 echo "Installing api.ayro.io certificate..."
 sudo certbot certonly -d api.ayro.io --standalone \
@@ -31,10 +31,10 @@ sudo certbot certonly -d api.ayro.io --standalone \
   --chain-path $AYRO_HOME/apps/ayro-configs/certs/api.ayro.io/chain.pem \
   --fullchain-path $AYRO_HOME/apps/ayro-configs/certs/api.ayro.io/fullchain.pem
 
-sudo ln -s /etc/letsencrypt/live/api.ayro.io/cert.pem $AYRO_HOME/apps/ayro-configs/certs/api.ayro.io/cert.pem
-sudo ln -s /etc/letsencrypt/live/api.ayro.io/privkey.pem $AYRO_HOME/apps/ayro-configs/certs/api.ayro.io/privkey.pem
-sudo ln -s /etc/letsencrypt/live/api.ayro.io/chain.pem $AYRO_HOME/apps/ayro-configs/certs/api.ayro.io/chain.pem
-sudo ln -s /etc/letsencrypt/live/api.ayro.io/fullchain.pem $AYRO_HOME/apps/ayro-configs/certs/api.ayro.io/fullchain.pem
+#sudo ln -s /etc/letsencrypt/live/api.ayro.io/cert.pem $AYRO_HOME/apps/ayro-configs/certs/api.ayro.io/cert.pem
+#sudo ln -s /etc/letsencrypt/live/api.ayro.io/privkey.pem $AYRO_HOME/apps/ayro-configs/certs/api.ayro.io/privkey.pem
+#sudo ln -s /etc/letsencrypt/live/api.ayro.io/chain.pem $AYRO_HOME/apps/ayro-configs/certs/api.ayro.io/chain.pem
+#sudo ln -s /etc/letsencrypt/live/api.ayro.io/fullchain.pem $AYRO_HOME/apps/ayro-configs/certs/api.ayro.io/fullchain.pem
 
 echo "Installing www.ayro.io certificate..."
 sudo certbot certonly -d www.ayro.io --standalone \
@@ -43,10 +43,10 @@ sudo certbot certonly -d www.ayro.io --standalone \
   --chain-path $AYRO_HOME/apps/ayro-configs/certs/www.ayro.io/chain.pem \
   --fullchain-path $AYRO_HOME/apps/ayro-configs/certs/www.ayro.io/fullchain.pem
 
-sudo ln -s /etc/letsencrypt/live/www.ayro.io/cert.pem $AYRO_HOME/apps/ayro-configs/certs/www.ayro.io/cert.pem
-sudo ln -s /etc/letsencrypt/live/www.ayro.io/privkey.pem $AYRO_HOME/apps/ayro-configs/certs/www.ayro.io/privkey.pem
-sudo ln -s /etc/letsencrypt/live/www.ayro.io/chain.pem $AYRO_HOME/apps/ayro-configs/certs/www.ayro.io/chain.pem
-sudo ln -s /etc/letsencrypt/live/www.ayro.io/fullchain.pem $AYRO_HOME/apps/ayro-configs/certs/www.ayro.io/fullchain.pem
+#sudo ln -s /etc/letsencrypt/live/www.ayro.io/cert.pem $AYRO_HOME/apps/ayro-configs/certs/www.ayro.io/cert.pem
+#sudo ln -s /etc/letsencrypt/live/www.ayro.io/privkey.pem $AYRO_HOME/apps/ayro-configs/certs/www.ayro.io/privkey.pem
+#sudo ln -s /etc/letsencrypt/live/www.ayro.io/chain.pem $AYRO_HOME/apps/ayro-configs/certs/www.ayro.io/chain.pem
+#sudo ln -s /etc/letsencrypt/live/www.ayro.io/fullchain.pem $AYRO_HOME/apps/ayro-configs/certs/www.ayro.io/fullchain.pem
 
 echo "Installing webcm.ayro.io certificate..."
 sudo certbot certonly -d webcm.ayro.io --standalone \
@@ -55,9 +55,9 @@ sudo certbot certonly -d webcm.ayro.io --standalone \
   --chain-path $AYRO_HOME/apps/ayro-configs/certs/webcm.ayro.io/chain.pem \
   --fullchain-path $AYRO_HOME/apps/ayro-configs/certs/webcm.ayro.io/fullchain.pem
 
-sudo ln -s /etc/letsencrypt/live/webcm.ayro.io/cert.pem $AYRO_HOME/apps/ayro-configs/certs/webcm.ayro.io/cert.pem
-sudo ln -s /etc/letsencrypt/live/webcm.ayro.io/privkey.pem $AYRO_HOME/apps/ayro-configs/certs/webcm.ayro.io/privkey.pem
-sudo ln -s /etc/letsencrypt/live/webcm.ayro.io/chain.pem $AYRO_HOME/apps/ayro-configs/certs/webcm.ayro.io/chain.pem
-sudo ln -s /etc/letsencrypt/live/webcm.ayro.io/fullchain.pem $AYRO_HOME/apps/ayro-configs/certs/webcm.ayro.io/fullchain.pem
+#sudo ln -s /etc/letsencrypt/live/webcm.ayro.io/cert.pem $AYRO_HOME/apps/ayro-configs/certs/webcm.ayro.io/cert.pem
+#sudo ln -s /etc/letsencrypt/live/webcm.ayro.io/privkey.pem $AYRO_HOME/apps/ayro-configs/certs/webcm.ayro.io/privkey.pem
+#sudo ln -s /etc/letsencrypt/live/webcm.ayro.io/chain.pem $AYRO_HOME/apps/ayro-configs/certs/webcm.ayro.io/chain.pem
+#sudo ln -s /etc/letsencrypt/live/webcm.ayro.io/fullchain.pem $AYRO_HOME/apps/ayro-configs/certs/webcm.ayro.io/fullchain.pem
 
 echo "Certificates installed with success!"
