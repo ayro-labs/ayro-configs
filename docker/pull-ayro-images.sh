1a3af2a702d508d53183279022e3402eba9e8824
Sandro Simas <sandro.csimas@gmail.com>
2017-12-07 22:33:58 -0300

    Fixing pull images script

--- docker/pull-ayro-images.sh
+++ docker/pull-ayro-images.sh
@@ -21,19 +21,13 @@ echo "Pulling images..."
 
 eval $(aws ecr get-login --no-include-email --region us-west-1)
 
-cd $AYRO_HOME/apps/ayro-configs/docker
-
-cat docker-compose-override.yml ayro/ayro/docker-compose.yml > docker-compose.yml
-docker-compose pull
-cat docker-compose-override.yml ayro/ayro-webcm/docker-compose.yml > docker-compose.yml
-docker-compose pull
-cat docker-compose-override.yml ayro/ayro-website/docker-compose.yml > docker-compose.yml
-docker-compose pull
-
+docker pull $AYRO_REGISTRY/ayro/ayro:latest
 docker tag $AYRO_REGISTRY/ayro/ayro:latest ayro:latest
-docker tag $AYRO_REGISTRY/ayro/ayro-webcm:latest ayro-webcm:latest
-docker tag $AYRO_REGISTRY/ayro/ayro-website:latest ayro-website:latest
 
-rm docker-compose.yml
+docker pull $AYRO_REGISTRY/ayro/ayro-webcm:latest
+docker tag $AYRO_REGISTRY/ayro/ayro-webcm:latest ayro-webcm:latest
+
+docker pull $AYRO_REGISTRY/ayro/ayro-website:latest
+docker tag $AYRO_REGISTRY/ayro/ayro-website:latest ayro-website:latest
 
 echo "Images pulled with success!"
