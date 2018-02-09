eb714f7689388b2215098427c04f318341e7527c
Ayro <ayro@ip-172-31-27-34.us-west-1.compute.internal>
2018-02-09 02:09:02 +0000

    Fixing cert cron tasks

--- certs/install-certs-tasks.sh
+++ certs/install-certs-tasks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -f
 
 if [ -z "$AYRO_HOME" ]; then
   echo "Enrivonment variable with name AYRO_HOME is required"
@@ -12,7 +13,9 @@ echo "Installing certificate cron tasks..."
 CRON_RENEW_CMD="$AYRO_HOME/apps/ayro-configs/certs/renew-certs.sh"
 CRON_RENEW_JOB="0 0 * * * $CRON_RENEW_CMD"
 
-(crontab -l | grep -v -F "$CRON_RENEW_CMD") | crontab -
-(crontab -l | grep -v -F "$CRON_RENEW_CMD" ; echo "$CRON_RENEW_JOB") | crontab -
+(crontab -l | grep -v -F "$CRON_RENEW_CMD") | crontab - || true
+(crontab -l | grep -v -F "$CRON_RENEW_CMD" ; echo "$CRON_RENEW_JOB") | crontab - || true
 
 echo "Certificate cron tasks was installed with success!"
+echo "Current tasks:"
+crontab -l
