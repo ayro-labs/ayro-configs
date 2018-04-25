bef2fd998723053745e596043c7d11f248793172
Sandro Simas <sandro.csimas@gmail.com>
2018-04-25 18:31:18 -0300

    Improve logging on renew-certs.sh

--- certs/renew-certs.sh
+++ certs/renew-certs.sh
@@ -13,16 +13,16 @@ echo "## Renewing certificates at $(date) ##" >> $AYRO_HOME/logs/renew-certs.log
 echo "###########################################################" >> $AYRO_HOME/logs/renew-certs.log
 echo "" >> $AYRO_HOME/logs/renew-certs.log
 
-echo "1º Renewing ayro.io certificate..."
+echo "1º Renewing ayro.io certificate..." >> $AYRO_HOME/logs/renew-certs.log
 sudo letsencrypt certonly -n --webroot -w $AYRO_HOME/data/letsencrypt -d ayro.io >> $AYRO_HOME/logs/renew-certs.log
 
-echo "2º Renewing api.ayro.io certificate..."
+echo "2º Renewing api.ayro.io certificate..." >> $AYRO_HOME/logs/renew-certs.log
 sudo letsencrypt certonly -n --webroot -w $AYRO_HOME/data/letsencrypt -d api.ayro.io >> $AYRO_HOME/logs/renew-certs.log
 
-echo "3º Renewing www.ayro.io certificate..."
+echo "3º Renewing www.ayro.io certificate..." >> $AYRO_HOME/logs/renew-certs.log
 sudo letsencrypt certonly -n --webroot -w $AYRO_HOME/data/letsencrypt -d www.ayro.io >> $AYRO_HOME/logs/renew-certs.log
 
-echo "4º Renewing webcm.ayro.io certificate..."
+echo "4º Renewing webcm.ayro.io certificate..." >> $AYRO_HOME/logs/renew-certs.log
 sudo letsencrypt certonly -n --webroot -w $AYRO_HOME/data/letsencrypt -d webcm.ayro.io >> $AYRO_HOME/logs/renew-certs.log
 
-echo "Certificates renewied with success!"
+echo "Certificates renewed with success!" >> $AYRO_HOME/logs/renew-certs.log
