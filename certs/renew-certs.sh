ed07498291e911501e7eed88b569abba5bf19534
Sandro Simas <sandro.csimas@gmail.com>
2018-06-10 13:30:09 -0300

    Adding some blank lines in renew-certs.sh to improve log file

--- certs/renew-certs.sh
+++ certs/renew-certs.sh
@@ -18,13 +18,17 @@ echo "" >> $AYRO_HOME/logs/renew-certs.log
 echo "1º Renewing ayro.io certificate..." >> $AYRO_HOME/logs/renew-certs.log
 sudo letsencrypt certonly -n --webroot -w $AYRO_HOME/data/letsencrypt -d ayro.io >> $AYRO_HOME/logs/renew-certs.log
 
+echo ""
 echo "2º Renewing api.ayro.io certificate..." >> $AYRO_HOME/logs/renew-certs.log
 sudo letsencrypt certonly -n --webroot -w $AYRO_HOME/data/letsencrypt -d api.ayro.io >> $AYRO_HOME/logs/renew-certs.log
 
+echo ""
 echo "3º Renewing www.ayro.io certificate..." >> $AYRO_HOME/logs/renew-certs.log
 sudo letsencrypt certonly -n --webroot -w $AYRO_HOME/data/letsencrypt -d www.ayro.io >> $AYRO_HOME/logs/renew-certs.log
 
+echo ""
 echo "4º Renewing webcm.ayro.io certificate..." >> $AYRO_HOME/logs/renew-certs.log
 sudo letsencrypt certonly -n --webroot -w $AYRO_HOME/data/letsencrypt -d webcm.ayro.io >> $AYRO_HOME/logs/renew-certs.log
 
+echo ""
 echo "Certificates renewed with success!" >> $AYRO_HOME/logs/renew-certs.log
